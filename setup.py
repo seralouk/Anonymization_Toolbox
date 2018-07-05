a93dacdf685bf51132a6b7f1bab5a35bafed8d0f
Serafeim Loukas <25902670+seralouk@users.noreply.github.com>
2018-07-05 14:45:28 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='Anonymization_Toolbox',
+      version="0.0.1",
+      description='A Python Module to anonymize',
+      long_description=description,
+      url='https://github.com/seralouk/Anonymization_Toolbox',
+      author='Serafeim Loukas',
+      author_email='seralouk@hotmail.com',
+      license='MIT',
+      packages=['Anonymous'],
+      install_requires=[
+          'pandas','numpy'  ],
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Topic :: Utilities",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python"
+      ],
+      zip_safe=False)
